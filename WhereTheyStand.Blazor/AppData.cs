a0426d267b46630c4d45361bf8fa3a2c02524d0c
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-05-12 19:55:02 -0700

    Add Total

--- WhereTheyStand.Blazor/AppData.cs
+++
@@ -1,9 +0,0 @@
-﻿using CreateDb;
-
-namespace WhereTheyStand.Blazor;
-
-public class AppData
-{
-    public Dictionary<Candidate, List<Donation>> candidateDict = new Dictionary<Candidate, List<Donation>>();
-    public Dictionary<Candidate, List<Donation>> pacDict = new Dictionary<Organization, List<Donation>>();
-}
