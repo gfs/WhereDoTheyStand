dddf84009d1aa0f1cfcdd0012139b837ef670e65
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-05-13 01:28:53 -0700

    Refactor data loading.

--- WhereTheyStand.Wasm/AppData.cs
+++ WhereTheyStand.Wasm/AppData.cs
@@ -1,11 +1,34 @@
-﻿using WhereTheyStand.Lib;
+﻿using Newtonsoft.Json;
+using System.Reflection;
+using WhereTheyStand.Lib;
 
 namespace WhereTheyStand.Wasm;
 
 public class AppData
 {
+    public bool IsLoaded { get; set; }
     public Dictionary<string, List<Donation>> candidateDict = new();
     public Dictionary<string, List<Donation>> pacDict = new();
     public Dictionary<string, Organization> pacMap = new();
     public Dictionary<string, Candidate> candidateMap = new();
+
+    public AppData()
+    {
+        
+    }
+    
+    public async Task LoadData()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var candidateText = await new StreamReader(assembly.GetManifestResourceStream("WhereTheyStand.Wasm.Candidates.json")).ReadToEndAsync();
+        var candidateMapText = await new StreamReader(assembly.GetManifestResourceStream("WhereTheyStand.Wasm.CandiateMap.json")).ReadToEndAsync();
+        var pacText = await new StreamReader(assembly.GetManifestResourceStream("WhereTheyStand.Wasm.SortedOrgs.json")).ReadToEndAsync();
+        var pacMapText = await new StreamReader(assembly.GetManifestResourceStream("WhereTheyStand.Wasm.PacMap.json")).ReadToEndAsync();
+
+        candidateDict = JsonConvert.DeserializeObject<Dictionary<string, List<Lib.Donation>>>(candidateText);
+        pacDict = JsonConvert.DeserializeObject<Dictionary<string, List<Lib.Donation>>>(pacText);
+        candidateMap = JsonConvert.DeserializeObject<Dictionary<string, Lib.Candidate>>(candidateMapText);
+        pacMap = JsonConvert.DeserializeObject<Dictionary<string, Lib.Organization>>(pacMapText);
+        IsLoaded = true;
+    }
 }
--- WhereTheyStand.Wasm/Client/Program.cs
+++ WhereTheyStand.Wasm/Client/Program.cs
@@ -10,22 +10,11 @@ namespace WhereTheyStand.Wasm
     {
         public static async Task Main(string[] args)
         {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            var candidateText = await new StreamReader(assembly.GetManifestResourceStream("WhereTheyStand.Wasm.Candidates.json")).ReadToEndAsync();
-            var candidateMap = await new StreamReader(assembly.GetManifestResourceStream("WhereTheyStand.Wasm.CandiateMap.json")).ReadToEndAsync();
-            var pacText = await new StreamReader(assembly.GetManifestResourceStream("WhereTheyStand.Wasm.SortedOrgs.json")).ReadToEndAsync();
-            var pacMap = await new StreamReader(assembly.GetManifestResourceStream("WhereTheyStand.Wasm.PacMap.json")).ReadToEndAsync();
-
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
             
             var appData = new AppData();
-
-            appData.candidateDict = JsonConvert.DeserializeObject<Dictionary<string, List<Lib.Donation>>>(candidateText);
-            appData.pacDict = JsonConvert.DeserializeObject<Dictionary<string, List<Lib.Donation>>>(pacText);
-            appData.candidateMap = JsonConvert.DeserializeObject<Dictionary<string, Lib.Candidate>>(candidateMap);
-            appData.pacMap = JsonConvert.DeserializeObject<Dictionary<string, Lib.Organization>>(pacMap);
 
             builder.Services.AddSingleton<AppData>(appData);
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
