d56e09cb100ac0cf4e8295e1450f9c8f0528a4b2
rjrabaca <105563550+rjrabaca@users.noreply.github.com>
2022-05-14 18:38:19 -0700

    Candidate ID Grabber
    Adds full data set from open secrets
    Adds tool to download data sets for 117th Congress Members

---
+++ Candidate ID Grabber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Candidate_ID_Grabber
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Regex Candidate_ID_Regex = new Regex("/members-of-congress/([a-z\\-]*)/summary\\?cid=(N[0-9]*)");
+            Dictionary<string, string> CandidateIds = new Dictionary<string, string>();
+            using HttpClient client = new HttpClient();
+            // Read the file and display it line by line.
+            foreach (string line in System.IO.File.ReadLines(@"C:\Users\ryanr\Downloads\opensecrets_members_of_congress_117.txt"))
+            {
+                MatchCollection matches = Candidate_ID_Regex.Matches(line);
+                if (matches.Count > 0)
+                {
+                    CandidateIds.Add(matches.First().Groups[2].Value, matches.First().Groups[1].Value);
+                }
+            }
+
+            Directory.CreateDirectory("Data");
+
+            foreach (var candidateID in CandidateIds)
+            {
+                var url = $"https://www.opensecrets.org/members-of-congress/contributors.csv?cid={candidateID.Key}&cycle=2022&recs=100&type=C";
+                var candidateCsv = await client.GetStringAsync(url);
+                // Wait 3000ms between fetches
+                File.WriteAllText(Path.Combine("Data", $"{candidateID.Value}-{candidateID.Key}-{DateTime.Now:yyyy-MM-dd}.csv"), candidateCsv);
+                Thread.Sleep(3000);
+                
+            }
+            System.Console.WriteLine("There were {0} IDs.", CandidateIds.Count);
+
+          
+        }
+    }
+}
